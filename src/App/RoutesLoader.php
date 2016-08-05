f9fe0e8483e213d84b67eee899aaba6e42e99be1
Alan Bondarchuk <imacoda@gmail.com>
2016-08-05 20:21:11 +0500

    Catch undefined routes, replace dots with slash in teamplte path

--- src/App/RoutesLoader.php
+++ src/App/RoutesLoader.php
@@ -20,15 +20,19 @@ class RoutesLoader {
     });
 
     $app->get('/partials/index', function (Application $app) {
-      return $app['twig']->render('partials.index.twig');
+      return $app['twig']->render('partials/index.twig');
     });
 
-    $app->get('/partials/{category}/{action?}', function (Application $app, Request $request, $category, $action = 'index') {
-      return $app['twig']->render(implode('.', ['partials', $category, $action]));
+    $app->get('/partials/{category}/{action?}', function (Application $app, $category, $action = 'index') {
+      return $app['twig']->render(implode('/', ['partials', $category, $action]));
     });
 
-    $app->get('/partials/{category}/{action}/{id}', function (Application $app, Request $request, $category, $action = 'index', $id) {
-      return $app['twig']->render(implode('.', ['partials', $category, $action]));
+    $app->get('/partials/{category}/{action}/{id}', function (Application $app, $category, $action = 'index', $id) {
+      return $app['twig']->render(implode('/', ['partials', $category, $action]));
     });
+
+    $app->get('{undefinedRoute}', function (Application $app, $undefinedRoute) {
+          return $app['twig']->render('layout.twig');
+    })->assert('undefinedRoute', '([A-z\d-\/_.]+)?');
   }
 }
