5eff28f4891bda2ce7d8c7547dfa9c8c78c33c73
Alan Bondarchuk <imacoda@gmail.com>
2016-08-06 02:20:16 +0500

    Improve routes and use {% verbatim %} for compatibility with angularjs tags

--- src/App/RoutesLoader.php
+++ src/App/RoutesLoader.php
@@ -4,35 +4,43 @@ namespace App;
 
 use Silex\Application;
 
-class RoutesLoader {
+class RoutesLoader
+{
 
-  private $app;
+    private $app;
 
-  public function __construct(Application $app) {
-    $this->app = $app;
-  }
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
 
-  public function bindRoutes() {
-    $app = $this->app;
+    public function bindRoutes()
+    {
+        $app = $this->app;
 
-    $app->get('/', function (Application $app) {
-      return $app['twig']->render('layout.twig');
-    });
+        $app->get('/', function (Application $app) {
+            return $app['twig']->render('layout.twig');
+        });
 
-    $app->get('/partials/index', function (Application $app) {
-      return $app['twig']->render('partials/index.twig');
-    });
+        $app->get('/partials/index', function (Application $app) {
+            return $app['twig']->render('partials/index.twig');
+        });
 
-    $app->get('/partials/{category}/{action?}', function (Application $app, $category, $action = 'index') {
-      return $app['twig']->render(implode('/', ['partials', $category, $action]));
-    });
+        $app->get('/partials/{category}/{action}',
+          function (Application $app, $category, $action) {
+              return $app['twig']->render(implode('/',
+                  ['partials', $category, $action]).'.twig');
+          })->value('action', 'index');
 
-    $app->get('/partials/{category}/{action}/{id}', function (Application $app, $category, $action = 'index', $id) {
-      return $app['twig']->render(implode('/', ['partials', $category, $action]));
-    });
+        $app->get('/partials/{category}/{action}/{id}',
+          function (Application $app, $category, $action = 'index', $id) {
+              return $app['twig']->render(implode('/',
+                  ['partials', $category, $action]).'.twig');
+          });
 
-    $app->get('{undefinedRoute}', function (Application $app, $undefinedRoute) {
-          return $app['twig']->render('layout.twig');
-    })->assert('undefinedRoute', '([A-z\d-\/_.]+)?');
-  }
+        $app->get('{undefinedRoute}',
+          function (Application $app, $undefinedRoute) {
+              return $app['twig']->render('layout.twig');
+          })->assert('undefinedRoute', '([A-z\d-\/_.]+)?');
+    }
 }
